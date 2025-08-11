074d3c608c25605bd70895033d9fab563161c2b1
Rodrigo Chaves <rodrigo.chaves@shuffleup.com>
2025-08-11 20:36:53 -0300

    feature Authentication - ajuste no security service para prover o método obteUsuarioLogado

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/usuario/security/SecurityService.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/usuario/security/SecurityService.java
@@ -15,10 +15,22 @@ public class SecurityService {
 
     private final UsuarioService usuarioService;
 
-    public Usuario obterUsuarioLogado(){
+    // alterado pois pegaremos o usuario logado do objeto Authentication
+    //    public Usuario obterUsuarioLogado(){
+    //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    //        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    //        String login = userDetails.getUsername();
+    //        return usuarioService.obterPorLogin(login);
+    //    }
+
+    public Usuario obterUsuarioLogado() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String login = userDetails.getUsername();
-        return usuarioService.obterPorLogin(login);
+
+        if (authentication instanceof CustomAuthentication customAuthentication) {
+            return customAuthentication.getUsuario();
+        }
+
+        return null;
     }
+
 }
